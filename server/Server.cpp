177b4afdbb52e31bd92031c745a1ac45acdaacb4
driely01 <elyaagoubi.works@gmail.com>
2024-03-02 21:59:05 +0100

    build a new server of a config file, but still not completed yet

---
+++ server/Server.cpp
@@ -0,0 +1,195 @@
+#include "Server.hpp"
+
+Server::Server( Config const &config ) {
+
+    this->yes = 1;
+    this->addrInfo = NULL;
+    this->newinfo = NULL;
+    this->servers = config.getServers();
+}
+
+Server::~Server( void ) { }
+
+void Server::getInfoaddr( std::string const &host, std::string const &port ) {
+
+    std::memset( &hints, 0, sizeof( hints ) );
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    this->status = getaddrinfo( host.c_str(), port.c_str(), &hints, &addrInfo );
+    if ( this->status == -1 ) {
+
+        std::cout << "error: getaddrinfo: " << gai_strerror( status ) << std::endl;
+        exit( EXIT_FAILURE );
+    }
+    printvalidoption( "getaddrinfo" );
+
+}
+
+int Server::createsocket( int &listener ) {
+
+    listener = socket( newinfo->ai_family, newinfo->ai_socktype, newinfo->ai_protocol );
+    if ( listener == -1 ) {
+
+        perror( "socket" );
+        return -1;
+    }
+    printvalidoption( "socket" );
+
+    if ( fcntl( listener, F_SETFL, O_NONBLOCK, FD_CLOEXEC ) == -1 ) {
+
+        perror( "fcntl" );
+        return -1;
+    }
+    printvalidoption( "fcntl" );
+
+    if ( setsockopt( listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof( int ) ) == -1 ) {
+
+        perror( "setsockopt" );
+        return -1;
+    }
+    printvalidoption( "setsockopt" );
+
+    return 0;
+}
+
+void Server::bindlistensock( int &listener, std::vector<Conf::Server>::iterator &it ) {
+
+    ( void ) it;
+    for ( newinfo = addrInfo; newinfo; newinfo = newinfo->ai_next ) {
+
+        if ( this->createsocket( listener ) == -1 )
+            continue;
+        
+        if ( bind( listener, newinfo->ai_addr, newinfo->ai_addrlen ) < 0 ) {
+
+            close( listener );
+            perror( "bind" );
+            continue;
+        }
+        printvalidoption( "bind" );
+        break;
+    }
+    freeaddrinfo( addrInfo );
+
+    if ( !newinfo ) {
+
+        printinvalidopt( "didn't get bound" );
+        exit( EXIT_FAILURE );
+    }
+
+    if ( listen( listener, BACKLOG ) == -1 ) {
+
+        perror( "listen" );
+        exit( EXIT_FAILURE );
+    }
+    printvalidoption( "listen" );
+    serverfds.insert( listener );
+}
+
+void Server::createServer( void ) {
+
+    int listener;
+
+    std::vector<Conf::Server>::iterator it = servers.begin();
+    for ( ; it != servers.end(); ++it ) {
+
+        // check if the host, port is already bound
+        if ( donehp.size() > 0 ) {
+
+            std::pair<std::string, std::string> search = std::make_pair( it->getListen().getHost(), it->getListen().getPort() );
+            std::set<std::pair<std::string, std::string> >::iterator it = donehp.find( search );
+            if ( it != donehp.end() )
+                continue;
+        }
+
+        // create socket and bind it
+        this->getInfoaddr( it->getListen().getHost(), it->getListen().getPort() );
+        this->bindlistensock( listener, it );
+
+        // add bound host, port to the container
+        donehp.insert( std::make_pair( it->getListen().getHost(), it->getListen().getPort() ) );
+        std::cout << DYELLO << "\tserver: " << it->getListen().getHost() << ":" << it->getListen().getPort() << RESET << std::endl;
+        std::cout << std::endl;
+    }
+    this->addpollservers();
+
+    std::vector<struct pollfd>::iterator itt = pfds.begin();
+    for ( ; itt != pfds.end(); ++itt ) {
+
+        std::cout << "pfd: " << itt->fd << std::endl;
+    }
+}
+
+void Server::addpollservers( void ) {
+
+    struct pollfd pfd;
+    std::set<int>::iterator it = serverfds.begin();
+    for ( ; it != serverfds.end(); ++it ) {
+
+        pfd.fd = *it;
+        pfd.events = POLLIN;
+        pfds.push_back( pfd );
+    }
+}
+
+void Server::addpollclients( int const &fd ) {
+
+    struct pollfd pfd;
+    pfd.fd = fd;
+    pfd.events = POLLIN;
+}
+
+void Server::mainpoll( void ) {
+
+    std::set<int>::iterator it;
+    if ( poll( pfds.data(), pfds.size(), -1 ) == -1 ) {
+
+        perror( "poll" );
+        exit( EXIT_FAILURE );
+    }
+
+    for ( int i = 0; i < pfds.size(); i++ ) {
+
+        it = serverfds.find( pfds[i].fd );
+        if ( pfds[i].revents == POLLIN ) {
+
+            if ( it != serverfds.end() ) {
+
+                // POLLIN revent in the serve side
+            } else {
+
+                // POLLIN revent int the clients side
+            }
+        } else if ( pfds[i].revents == POLLOUT ) {
+
+            if ( it != serverfds.end() ) {
+
+                // POLLOUT revent in the server side
+            } else {
+
+                // POLLOUT revents in the clients side
+            }
+        } else if ( pfds[i].revents == POLLHUP ) {
+
+            if ( it != serverfds.end() ) {
+
+                // POLLHUP revent in the server side
+            } else {
+
+                // POLLHUP revent in the clients side
+            }
+        }
+    }
+}
+
+void printvalidoption( std::string const &str ) {
+
+    std::cout << GREEN << "\t--> " << str << RESET << std::endl;
+}
+
+void printinvalidopt( std::string const &str ) {
+
+    std::cout << RED << "\t" << str << RESET << std::endl;
+}
---
+++ server/Server.hpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 13:26:06 by del-yaag          #+#    #+#             */
+/*   Updated: 2024/03/02 21:41:09 by del-yaag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <set>
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <sys/fcntl.h>
+#include <errno.h>
+
+#include "Colors.hpp"
+#include "../Server.hpp"
+#include "../Config.hpp"
+
+#define SEND 1024
+#define BACKLOG 128
+
+class Server {
+
+    private:
+        std::set<int> serverfds;
+        std::set<std::pair<std::string, std::string> > donehp;
+        int yes;
+        int status;
+        
+        struct addrinfo hints;
+        struct addrinfo *addrInfo;
+        struct addrinfo *newinfo;
+
+        // accept
+        // struct sockaddr_storage ramoteaddr;
+        // socklen_t addrlen;
+        // char remoteip[INET6_ADDRSTRLEN];
+        
+        // // poll
+        std::vector<struct pollfd> pfds;
+
+        std::vector<Conf::Server> servers;
+
+
+        // members methods
+        void getInfoaddr( std::string const &host, std::string const &port );
+        int createsocket( int &listener );
+        void bindlistensock( int &listener, std::vector<Conf::Server>::iterator &it );
+        void addpollservers( void );
+        void addpollclients( int const &fd );
+        void mainpoll( void );
+    
+    public:
+        Server( Config const &config );
+        ~Server( void );
+
+        void createServer( void );
+
+};
+
+void printvalidoption( std::string const &str );
+void printinvalidopt( std::string const &str );
